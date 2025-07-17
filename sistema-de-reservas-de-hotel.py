bf695273d4d04f4896dc64f8d9f3ec10946aee29
Maxwel-2024 <maxwelsena@gmail.com>
2025-07-16 23:49:24 -0300

    sistema-de-reservas-de-hotel

---
+++ sistema-de-reservas-de-hotel.py
@@ -0,0 +1,25 @@
+def processar_reservas():
+    # Entrada dos quartos disponíveis
+    quartos_disponiveis = set(map(int, input().split()))
+    
+    # Entrada das reservas solicitadas
+    reservas_solicitadas = list(map(int, input().split()))
+
+    # Listas para armazenar resultados
+    confirmadas = []
+    recusadas = []
+
+    # Processamento das reservas
+    for reserva in reservas_solicitadas:
+        if reserva in quartos_disponiveis:
+            confirmadas.append(reserva)
+            quartos_disponiveis.remove(reserva)  # Remove para não reservar de novo
+        else:
+            recusadas.append(reserva)
+
+    # Saída dos resultados conforme especificação
+    print("Reservas confirmadas:", " ".join(map(str, confirmadas)))
+    print("Reservas recusadas:", " ".join(map(str, recusadas)))
+
+# Chamada da função principal
+processar_reservas()
